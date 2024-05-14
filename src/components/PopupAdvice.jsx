82eaf1183591ab9c6cdacae9a9951572d199fba2
Meyyaou <104941102+Meyyaou@users.noreply.github.com>
2024-05-14 20:35:54 +0100

    Update PopupAdvice.jsx

--- src/components/PopupAdvice.jsx
+++ src/components/PopupAdvice.jsx
@@ -1,72 +1,18 @@
-.popup {
-  z-index: 6;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.2);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+import React from "react";
+import "./PopupAdvice.css";
+function PopupAdvice(props) {
+  return props.trigger ? (
+    <div className="popup">
+      <div className="popup-inner">
+        <button className="close-popup-btn" onClick={() => props.setTrigger(false)}>
+          close
+        </button>
+        {props.children}
+      </div>
+    </div>
+  ) : (
+    ""
+  );
 }
 
-.popup-inner {
-  position: relative;
-  padding: 32px;
-  width: 100%;
-  max-width: 640px;
-  background-color: #fff;
-}
-.popup-inner .close-popup-btn {
-  position: absolute;
-  top: 16px;
-  right: 16px;
-}
-
-.close-popup-btn {
-  background-color: #ffffff;
-  border: 1px solid #222222;
-  border-radius: 8px;
-  box-sizing: border-box;
-  color: #222222;
-  cursor: pointer;
-  display: inline-block;
-  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto,
-    "Helvetica Neue", sans-serif;
-  font-size: 16px;
-  font-weight: 600;
-  line-height: 20px;
-  margin: 0;
-  outline: none;
-  padding: 13px 23px;
-  position: relative;
-  text-align: center;
-  text-decoration: none;
-  touch-action: manipulation;
-  transition: box-shadow 0.2s, -ms-transform 0.1s, -webkit-transform 0.1s,
-    transform 0.1s;
-  user-select: none;
-  -webkit-user-select: none;
-  width: auto;
-}
-
-.close-popup-btn:focus-visible {
-  box-shadow: #222222 0 0 0 2px, rgba(255, 255, 255, 0.8) 0 0 0 4px;
-  transition: box-shadow 0.2s;
-}
-.close-popup-btn:active {
-  background-color: #f7f7f7;
-  border-color: #000000;
-  transform: scale(0.96);
-}
-
-.close-popup-btn:disabled {
-  border-color: #dddddd;
-  color: #dddddd;
-  cursor: not-allowed;
-  opacity: 1;
-}
-.close-popup-btn:hover {
-  background-color: #efe5e5;
-}
+export default PopupAdvice;
