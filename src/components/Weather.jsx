23757f68df7dce47eceeb54bde010ef0627e8807
Meyyaou <104941102+Meyyaou@users.noreply.github.com>
2024-05-14 20:38:36 +0100

    Update Weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -8,53 +8,55 @@ const Weather = ({ city }) => {
   const [weatherData, setWeatherData] = useState(null);
   const [icon, setIcon] = useState(null);
 
-
   useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=a3d9fc1db3e6de26733e215512444a2b4
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=3258acf3613370332f9694e6267b94a5
         `
-      );
-      
-      setWeatherData(response.data);
-      const temp = response.data.main.temp;
-      if (temp < 10) {
-        setIcon(moon); 
-      } else if (temp >= 10 && temp < 20) {
-        setIcon(cloudy); 
-      } else if (temp >= 20 && temp < 30) {
-        setIcon(sunny); 
-      } else {
-        setIcon(lightening); 
-      }
-      console.log(response.data); 
-    } catch (error) {
-      console.error(error);
-    }
-  };
+        );
 
- 
+        setWeatherData(response.data);
+        const temp = response.data.main.temp;
+        if (temp < 10) {
+          setIcon(moon);
+        } else if (temp >= 10 && temp < 20) {
+          setIcon(cloudy);
+        } else if (temp >= 20 && temp < 30) {
+          setIcon(sunny);
+        } else {
+          setIcon(lightening);
+        }
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
     fetchData();
   }, []);
 
-
-
   return (
-    
-    <div>
-      {weatherData ? 
-      (
-        
+    <>
+      {weatherData ? (
         <>
-          <img src={icon} style={{marginTop: 10, marginBottom: 0, height: '50px', width: '50px', cursor: "auto"}} />
-          <h2>{weatherData.name}</h2>
+          <img
+            src={icon}
+            style={{
+              marginTop: 10,
+              marginBottom: 0,
+              height: "50px",
+              width: "50px",
+              cursor: "auto",
+            }}
+          />
+          <h2 style={{fontSize: "1.6em"}}>{weatherData.name}</h2>
           <p>{weatherData.main.temp}°C</p>
         </>
       ) : (
-        <p>Loading weather data...</p>
+        <p style={{fontSize: "1.2em"}}>Loading weather data...</p>
       )}
-    </div>
+    </>
   );
 };
 
