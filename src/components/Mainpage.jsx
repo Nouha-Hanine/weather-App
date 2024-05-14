bbc07e4e913b2b0679350399688442cc8e99909c
Meyyaou <104941102+Meyyaou@users.noreply.github.com>
2024-05-14 20:34:43 +0100

    Update Mainpage.jsx

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -1,17 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import "./Mainpage.css";
+import InfoWidget from "./InfoWidget.jsx";
 import AdviceButton from "./AdviceButton.jsx";
 import Background from "./Background.jsx";
-import Header from "./Header.jsx";
 import WeatherCard from "./WeatherCard.jsx";
 import DisplayBluetooth from "./DisplayBluetooth.jsx";
 import AroundConstantine from "./AroundConstantine.jsx";
 import PredictCard from "./PredictCard.jsx";
 import GeneralAdvice from "./GeneralAdvice.jsx";
 import Greetings from "./Greetings.jsx";
-function Mainpage() {
+import SunhourWidget from "./SunhourWidget.jsx";
+function Mainpage({user}) {
+  const [userName, setUserName] = useState('');
+  const [alertEnabled, setAlertEnabled] = useState(false);
+  const [newPassword, setNewPassword] = useState('');
+  const [selectedType, setSelectedType] = useState('');
+  const [userId, setUserId] = useState('');
+  const [userEmail, setUserEmail] = useState(''); 
+  const [userPassword, setUserPassword] = useState(''); 
+  
+  useEffect(() => {
+    if (user) {
+      setUserId(user._id);
+      setUserName(user.name);
+      setAlertEnabled(user.alerts === 'enable');
+      setSelectedType(user.type);
+      setUserEmail(user.email); 
+      setUserPassword(user.password); 
+    }
+  }, [user]);
   //TODO need to add use state par rapport au background
-  //TODO must add heat index and dew point (from temperature and humidity)
   //TODO add usestatecontext for weather display?
   const [backgroundState, setBackgroundState] = useState("cloudy");
   const [dataAvailable, setDataAvailable] = useState(true);
@@ -21,12 +39,12 @@ function Mainpage() {
     <div>
       {" "}
       
-      <Greetings />
+      <Greetings user={user} />
       {!dataAvailable && <DisplayBluetooth />}
       {dataAvailable && (
         <div>
-          <Background backgroundState="sunny" />
-          <AdviceButton />
+          <Background backgroundState="cloudy" />
+          <AdviceButton user={user} />
           <main className="weather-card-container">
             <WeatherCard
               temperature="20"
@@ -37,9 +55,14 @@ function Mainpage() {
           </main>
         </div>
       )}
+      <InfoWidget />
+      <SunhourWidget />
+      <div className="blank-bloc"></div>
       <GeneralAdvice />
+
+      
       <div className="second-part-page">
-        <AroundConstantine temperature={9} />
+        <AroundConstantine/>
 
         <PredictCard />
       </div>
