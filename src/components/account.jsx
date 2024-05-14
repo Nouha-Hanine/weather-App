c544a86e25b4e4855c7809c6bce6723707ea551f
Nouha-Hanine <153952572+Nouha-Hanine@users.noreply.github.com>
2024-05-14 13:59:39 +0100

    Update account.jsx

--- src/components/account.jsx
+++ src/components/account.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './account.css';
 
-function Account({ onClose }) {
-  const [userName, setUserName] = useState('username');
+function Account({ user, onClose }) {
+  const [userName, setUserName] = useState('');
   const [alertEnabled, setAlertEnabled] = useState(false);
   const [oldPassword, setOldPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
-  
-  const [selectedType, setSelectedType] = useState('Type A');
-  
+  const [selectedType, setSelectedType] = useState('');
   const [isEditingName, setIsEditingName] = useState(false);
+  const [userId, setUserId] = useState('');
+  const [userEmail, setUserEmail] = useState(''); 
+  const [userPassword, setUserPassword] = useState(''); 
+  
+  useEffect(() => {
+    if (user) {
+      setUserId(user._id);
+      setUserName(user.name);
+      setAlertEnabled(user.alerts === 'enable');
+      setSelectedType(user.type);
+      setUserEmail(user.email); 
+      setUserPassword(user.password); 
+    }
+  }, [user]);
 
   const handleUserNameChange = (e) => {
     setUserName(e.target.value);
@@ -27,26 +39,54 @@ function Account({ onClose }) {
     setNewPassword(e.target.value);
   };
 
- 
   const handleTypeChange = (e) => {
     setSelectedType(e.target.value);
   };
 
   const handleUserNameClick = () => {
-    setIsEditingName(true); 
+    setIsEditingName(true);
   };
 
-  
-
   const handleUserNameBlur = () => {
-    setIsEditingName(false); 
+    setIsEditingName(false);
   };
 
   const handleSave = () => {
-   
-    console.log('User name updated:', userName);
-  };
+    if (oldPassword && oldPassword !== userPassword) {
+      alert('Old password is incorrect.');
+      return;
+    }
+    const userDataToUpdate = {
+      name: userName,
+      alerts: alertEnabled ? 'enable' : 'disable',
+      type: selectedType,
+      email: userEmail, 
+      password: newPassword ? newPassword : userPassword,
+    };
 
+    fetch(`http://localhost:5000/updateUserByEmail/${userEmail}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userDataToUpdate),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('User data updated successfully:', data);
+        setUserPassword(newPassword ? newPassword : userPassword);
+        
+      })
+      .catch((error) => {
+        console.error('Error updating user data:', error);
+        
+      });
+  };
 
   return (
     <div className="account-container">
@@ -54,38 +94,38 @@ function Account({ onClose }) {
         X
       </button>
       <div className="profile-section">
-       
         {/* Nom modifiable */}
         <div className="profile-name" onClick={handleUserNameClick}>
           <label htmlFor="userName">Name:</label>
-          {isEditingName ? ( 
+          {isEditingName ? (
             <input
               type="text"
               id="userName"
               value={userName}
               onChange={handleUserNameChange}
               onBlur={handleUserNameBlur}
-              autoFocus 
+              autoFocus
             />
           ) : (
-            
             <span>{userName}</span>
           )}
         </div>
       </div>
       {/* Section Alertes */}
       <div className="alert-section">
-        
         <label>Alerts:</label>
-        <button className={alertEnabled ? "enable-button" : "disable-button"} onClick={handleAlertToggle}>
-          {alertEnabled ? 'Enable' : 'Disable'}
+        <button
+          className={alertEnabled ? 'enable-button' : 'disable-button'}
+          onClick={handleAlertToggle}
+        >
+          {alertEnabled ? 'Enabled' : 'Disabled'}
         </button>
       </div>
       {/* Section Changement de mot de passe */}
       <div className="password-section">
         <label htmlFor="oldPassword">Old Password:</label>
         <input
-        placeholder='Enter your password'
+          placeholder="Enter your password"
           type="password"
           id="oldPassword"
           value={oldPassword}
@@ -93,22 +133,20 @@ function Account({ onClose }) {
         />
         <label htmlFor="newPassword">New Password:</label>
         <input
-        placeholder='Enter new password'
+          placeholder="Enter new password"
           type="password"
           id="newPassword"
-          value={newPassword}
           onChange={handleNewPasswordChange}
         />
-       
       </div>
       {/* Section Choix de type */}
       <div className="type-section">
         <label htmlFor="type">Type:</label>
         <select id="type" value={selectedType} onChange={handleTypeChange}>
-          <option value="Type A">student</option>
-          <option value="Type B">airline-worker</option>
-          <option value="Type C">agriculture-worker</option>
-          <option value="Type D">driver</option>
+          <option value="student">Student</option>
+          <option value="airline-worker">Airline Worker</option>
+          <option value="agriculture-worker">Agriculture Worker</option>
+          <option value="driver">Driver</option>
         </select>
       </div>
       {/* Bouton de sauvegarde */}
@@ -120,5 +158,6 @@ function Account({ onClose }) {
 }
 
 export default Account;
+
 
 
