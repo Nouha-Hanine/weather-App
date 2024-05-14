1495397eb968e178eef7a1a6aee10499d07450df
Nouha-Hanine <153952572+Nouha-Hanine@users.noreply.github.com>
2024-05-14 13:58:45 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './components/Login';
 import Signup from './components/Signup';
@@ -7,46 +7,49 @@ import Account from './components/account';
 import Header from './components/Header';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('login'); 
+  const [currentPage, setCurrentPage] = useState('login');
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const handleLoginSuccess = (userData) => {
+    setLoggedInUser(userData);
+    navigateTo('mainpage');
+  };
+
   useEffect(() => {
     const storedPage = localStorage.getItem('currentPage');
     if (storedPage) {
       setCurrentPage(storedPage);
     }
   }, []);
+
   const navigateTo = (page) => {
     setCurrentPage(page);
     localStorage.setItem('currentPage', page);
   };
 
- 
   const renderHeader = () => {
     if (currentPage !== 'login' && currentPage !== 'signup' && currentPage !== 'account') {
-      return <Header navigateTo={navigateTo} />;
+      return <Header navigateTo={navigateTo} onLogout={handleLogout} />;
     }
-    return null; 
+    return null;
   };
 
   const handleCloseAccount = () => {
-    setCurrentPage('mainpage'); 
+    setCurrentPage('mainpage');
   };
 
   const handleLogout = () => {
-    setCurrentPage('login'); 
+    setLoggedInUser(null);
+    setCurrentPage('login');
   };
 
   return (
     <div className="App">
-    
       {renderHeader()}
-
-    
-      {currentPage === 'login' && <Login navigateTo={navigateTo} />}
+      {currentPage === 'login' && <Login onLoginSuccess={handleLoginSuccess} navigateTo={navigateTo} />}
       {currentPage === 'signup' && <Signup navigateTo={navigateTo} />}
       {currentPage === 'mainpage' && <Mainpage navigateTo={navigateTo} />}
-      {currentPage === 'account' && <Account onClose={handleCloseAccount} />}
-
-      
+      {currentPage === 'account' && <Account user={loggedInUser} onClose={handleCloseAccount} />}
     </div>
   );
 }
