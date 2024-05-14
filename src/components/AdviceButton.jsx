70c3f470ac0f903cdf0b8e2f4a2faaea144f6efb
Meyyaou <104941102+Meyyaou@users.noreply.github.com>
2024-05-14 20:30:55 +0100

    Update AdviceButton.jsx

--- src/components/AdviceButton.jsx
+++ src/components/AdviceButton.jsx
@@ -1,39 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Tooltip from "@mui/material/Tooltip";
 import IconButton from "@mui/material/IconButton";
 import LightbulbIcon from "@mui/icons-material/Lightbulb";
 import PopupAdvice from "./PopupAdvice";
-import {motion} from 'framer-motion';
+import axios from 'axios';
 
-function AdviceButton() {
+function AdviceButton({user}) {
+  const [advice, setAdvice] = useState("no advice");
   const [popupButton, setPopupButton] = useState(false);
- /**TODO arrange the animation */
+ const [userName, setUserName] = useState('');
+  const [alertEnabled, setAlertEnabled] = useState(false);
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [selectedType, setSelectedType] = useState('');
+  const [isEditingName, setIsEditingName] = useState(false);
+  const [userId, setUserId] = useState('');
+  const [userEmail, setUserEmail] = useState(''); 
+  const [userPassword, setUserPassword] = useState(''); 
+  
+  useEffect(() => {
+    if (user) {
+      setUserId(user._id);
+      setUserName(user.name);
+      setAlertEnabled(user.alerts === 'enable');
+      setSelectedType(user.type);
+      setUserEmail(user.email); 
+      setUserPassword(user.password); 
+    }
+  }, [user]);
+
+  
+  const fetchRandomAdvice = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/randomAdvice/${selectedType}`);
+      if (response.status !== 200) { // Check status directly
+        throw new Error('Network response was not ok');
+      }
+      setAdvice(response.data);
+    } catch (error) {
+      console.log("type is in here : ", selectedType);
+      console.error('Error fetching random advice:', error);
+    }
+  };
+  
+
+  
+
   return (
-   <div>
-    <Tooltip
-    title="Get an advice" arrow
-  style={{
-    zIndex: 3,
-    textAlign: "center",
-    position: "fixed",
-    right: 30,
-    bottom: 30,
-    backgroundColor: "white", 
-    
-  }}
->
-  <IconButton
-    color="info"
-    aria-label="get-advice"
-    onClick={() => setPopupButton(true)}
-  >
-    <LightbulbIcon />
-  </IconButton>
-</Tooltip>
-    
+    <div>
+      <Tooltip
+        title="Get an advice" arrow
+        style={{
+          zIndex: 3,
+          textAlign: "center",
+          position: "fixed",
+          right: 30,
+          bottom: 30,
+          backgroundColor: "white", 
+        }}
+      >
+        <IconButton
+          color="info"
+          aria-label="get-advice"
+          onClick={() => {
+            fetchRandomAdvice();
+            setPopupButton(true);
+          }}
+        >
+          <LightbulbIcon />
+        </IconButton>
+      </Tooltip>
+        
       <PopupAdvice trigger={popupButton} setTrigger={setPopupButton}>
-        <h3 style={{ color: "black" }}>my popup</h3>
-        <p style={{ color: "black" }}>this will soon become an advice for user!</p>
+        <h3 style={{ color: "black" }}>{selectedType}</h3>
+        <p style={{ color: "black" }}>{advice}</p>
       </PopupAdvice>
     </div>
   );
